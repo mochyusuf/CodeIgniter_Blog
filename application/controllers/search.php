fb9b4cc27b4d4506c69d5c1b3823a43f0dae602a
Mochamad Yusuf <mochyusuf100@gmail.com>
2018-06-30 14:10:49 +0700

    Update Lock

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -1,6 +1,5 @@
 <?php
 
-    
     defined('BASEPATH') OR exit('No direct script access allowed');
     
     class search extends CI_Controller {
---
+++ application/controllers/tes.php
@@ -0,0 +1,16 @@
+<?php
+    
+    defined('BASEPATH') OR exit('No direct script access allowed');
+    
+    class tes extends CI_Controller {
+    
+        public function index()
+        {
+            $this->load->view('tes.php');
+            
+        }
+    
+    }
+    
+    /* End of file tes.php */
+    
